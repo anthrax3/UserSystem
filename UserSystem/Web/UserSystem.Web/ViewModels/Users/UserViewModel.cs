94d77c5d28f0f1004469e37e73d1e4f9e5f8a423
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-10 23:14:35 +0300

    Created UserViewModel

---
+++ UserSystem/Web/UserSystem.Web/ViewModels/Users/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace UserSystem.Web.ViewModels.Users
+{
+    using UserSystem.Data.Models;
+    using UserSystem.Web.Infrastructure.Mapping;
+
+    public class UserViewModel : IMapFrom<User>
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Gender Sex { get; set; }
+
+        public string Telephone { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
