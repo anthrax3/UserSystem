c6daf525d5b5eb26cad0f64ee9a95f4334172138
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-10 17:00:24 +0300

    Created ApplicationDbContext

---
+++ UserSystem/Data/UserSystem.Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace UserSystem.Data
+{
+    using System.Data.Entity;
+
+    using UserSystem.Data.Models;
+
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public IDbSet<User> Users { get; set; }
+    }
+}
