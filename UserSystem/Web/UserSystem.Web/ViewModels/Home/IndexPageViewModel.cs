3276932e69ed5b474b071a706983cac9bb6e32e7
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-10 21:07:39 +0300

    Created IndexPageViewModel

---
+++ UserSystem/Web/UserSystem.Web/ViewModels/Home/IndexPageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UserSystem.Web.ViewModels.Home
+{
+    using System.Collections.Generic;
+
+    public class IndexPageViewModel
+    {
+        public IEnumerable<UserConciseViewModel> Users { get; set; }
+    }
+}
