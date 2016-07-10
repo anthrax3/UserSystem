0d6ce049dafd0894b693ae4a8ab4441161fc3d1b
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-10 23:55:04 +0300

    Implemented edit user profile

--- UserSystem/Web/UserSystem.Web/Controllers/UsersController.cs
+++ UserSystem/Web/UserSystem.Web/Controllers/UsersController.cs
@@ -6,6 +6,7 @@
 
     using AutoMapper.QueryableExtensions;
 
+    using UserSystem.Data.Models;
     using UserSystem.Data.UnitOfWork;
     using UserSystem.Web.ViewModels.Users;
 
@@ -32,6 +33,38 @@
             var model = this.Data.Users.All().Where(u => u.Id == id).ProjectTo<UserViewModel>().FirstOrDefault();
 
             return this.View(model);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = this.Data.Users.Find(id);
+            if (user == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            return this.View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(
+            [Bind(Include = "Id,Username,Email,FirstName,LastName,Sex,Telephone,PhotoUrl,Status")] User user)
+        {
+            if (this.ModelState.IsValid)
+            {
+                this.Data.Users.Update(user);
+                this.Data.SaveChanges();
+
+                return this.RedirectToAction("Profile", "Users", new { area = string.Empty, id = user.Id });
+            }
+
+            return this.View(user);
         }
     }
 }
