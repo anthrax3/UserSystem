418d436c144be9d85ec6f90a3a7426907566907a
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-10 19:05:22 +0300

    Created BaseController and HomeController
    Added bootstrap, jquery and some fonts

---
+++ UserSystem/Web/UserSystem.Web/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿namespace UserSystem.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using UserSystem.Data.UnitOfWork;
+
+    public class BaseController : Controller
+    {
+        protected BaseController(IUserSystemData data)
+        {
+            this.Data = data;
+        }
+
+        public IUserSystemData Data { get; }
+    }
+}
---
+++ UserSystem/Web/UserSystem.Web/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿namespace UserSystem.Web.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using UserSystem.Data.UnitOfWork;
+
+    public class HomeController : BaseController
+    {
+        public HomeController(IUserSystemData data)
+            : base(data)
+        {
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
