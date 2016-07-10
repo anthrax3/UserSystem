4808d00df71e47d8de02c6c9edaa61e52025fb09
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-10 16:24:01 +0300

    Using statements sorted and moved inside namespace declaration

--- UserSystem/Web/UserSystem.Web/Global.asax.cs
+++ UserSystem/Web/UserSystem.Web/Global.asax.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
-using System.Web.Http;
-
-namespace UserSystem.Web
+﻿namespace UserSystem.Web
 {
+    using System;
+    using System.Web;
+    using System.Web.Http;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
     public class Global : HttpApplication
     {
         void Application_Start(object sender, EventArgs e)
@@ -17,7 +13,7 @@ namespace UserSystem.Web
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
