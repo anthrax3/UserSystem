77db9e5301757df4f1d158bd3e0db46304c4194a
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-10 16:47:22 +0300

    Created user, gender and status model

---
+++ UserSystem/Data/UserSystem.Data.Models/Status.cs
@@ -0,0 +1,9 @@
+﻿namespace UserSystem.Data.Models
+{
+    public enum Status
+    {
+        Active = 0, 
+        Inactive = 1, 
+        Deleted = 2
+    }
+}
---
+++ UserSystem/Data/UserSystem.Data.Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserSystem.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Gender Sex { get; set; }
+
+        public string Telephone { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
