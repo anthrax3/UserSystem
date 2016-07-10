78e8234ceafa4166ba235278e5045a214c6f2756
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-10 17:11:02 +0300

    Implemented Unit of Work Pattern

---
+++ UserSystem/Data/UserSystem.Data/UnitOfWork/IUserSystemData.cs
@@ -0,0 +1,12 @@
+﻿namespace UserSystem.Data.UnitOfWork
+{
+    using UserSystem.Data.Models;
+    using UserSystem.Data.Repositories;
+
+    public interface IUserSystemData
+    {
+        IRepository<User> Users { get; }
+
+        int SaveChanges();
+    }
+}
---
+++ UserSystem/Data/UserSystem.Data/UnitOfWork/UserSystemData.cs
@@ -0,0 +1,40 @@
+﻿namespace UserSystem.Data.UnitOfWork
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+
+    using UserSystem.Data.Models;
+    using UserSystem.Data.Repositories;
+
+    public class UserSystemData : IUserSystemData
+    {
+        private readonly DbContext dbContext;
+
+        private readonly IDictionary<Type, object> repositories;
+
+        public UserSystemData(DbContext context)
+        {
+            this.dbContext = context;
+            this.repositories = new Dictionary<Type, object>();
+        }
+
+        public IRepository<User> Users => this.GetRepository<User>();
+
+        public int SaveChanges()
+        {
+            return this.dbContext.SaveChanges();
+        }
+
+        private IRepository<T> GetRepository<T>() where T : class
+        {
+            if (!this.repositories.ContainsKey(typeof(T)))
+            {
+                var type = typeof(GenericEfRepository<T>);
+                this.repositories.Add(typeof(T), Activator.CreateInstance(type, this.dbContext));
+            }
+
+            return (IRepository<T>)this.repositories[typeof(T)];
+        }
+    }
+}
