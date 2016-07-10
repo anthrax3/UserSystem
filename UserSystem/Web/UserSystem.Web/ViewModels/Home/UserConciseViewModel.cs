157b17fbb3b6c18341981d22d9e67d32b2d66719
deyanstoyanov <deyan.stoyanov.m@gmail.com>
2016-07-10 19:12:49 +0300

    Created UserConciseViewModel

---
+++ UserSystem/Web/UserSystem.Web/ViewModels/Home/UserConciseViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace UserSystem.Web.ViewModels.Home
+{
+    using UserSystem.Data.Models;
+
+    public class UserConciseViewModel
+    {
+        public string Username { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
